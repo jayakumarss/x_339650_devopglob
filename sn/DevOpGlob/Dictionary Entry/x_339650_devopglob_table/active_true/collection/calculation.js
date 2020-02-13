819dbf5c49650949499f84c458601d8fbd828621
jayakumarss <jayakumar.sengodan@gmail.com>
2020-02-13 15:31:33 -0500

    'master' branch updated from  https://dev64721.service-now.com no-cicd

---
+++ sn/DevOpGlob/Dictionary Entry/x_339650_devopglob_table/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : DevOpGlob
+ * ClassName   : sys_dictionary
+ * Created On  : 2020-02-13 20:29:19
+ * Created By  : admin
+ * Updated On  : 2020-02-13 20:29:19
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=878711c0db430010ab0a4870399619e5
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
